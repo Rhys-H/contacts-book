9a999d0573d6297d89b27284e00f2b4e415bc5c7
Rhys-H <rhys.howells01@gmail.com>
2024-09-10 18:30:13 +0100

    feat: basic contacts list
    With the ability to add, list, and delete contacts.

---
+++ cmd/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+var id = 0
+
+type Contact struct {
+	Id    int
+	Name  string
+	Email string
+}
+
+func newContact(name, email string) Contact {
+	id++
+	return Contact{
+		Id:    id,
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+	return -1
+}
+
+func (d *Data) emailExists(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("John", "john@example.com"),
+			newContact("Jane", "jane@example.com"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	page := newPage()
+	e.Renderer = newTemplate()
+	e.Use(middleware.Logger())
+
+	e.Static("/images", "images")
+	e.Static("/css", "css")
+	e.Static("/js", "js")
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index.html", page)
+	})
+
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.emailExists(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+
+			formData.Errors["email"] = "Email already exists"
+			return c.Render(422, "contact-form", formData)
+		}
+
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+
+		err := c.Render(200, "contact-form", newFormData())
+
+		if err != nil {
+			return err
+		}
+
+		return c.Render(200, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+
+		if err != nil {
+			return c.String(400, "Invalid ID")
+		}
+
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(404, "Contact not found")
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+		return c.NoContent(200)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
